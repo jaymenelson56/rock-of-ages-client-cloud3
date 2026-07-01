8ca00e38d9de943bce91b05c47d86faf3a3cbdea
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2026-07-01 13:33:51 -0500

    added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = <your-state-bucket-name>  
+    bucket         = "rock-of-ages-image-storing-bucket-jnmh"  
     key            = "client/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "rock-of-ages-terraform-locks"  
